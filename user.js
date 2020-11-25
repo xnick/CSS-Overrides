94fa8ae7287e4be1731c5eb3c425542e93158791
xnick <xnick@users.noreply.github.com>
2020-11-25 20:32:58 +0200

    Create user.js

---
+++ user.js
@@ -0,0 +1,12 @@
+//Smooth Scrolling, theoretically --Source? https://www.reddit.com/r/firefox/comments/bvfqtp/these_are_the_smooth_scrolling_tweaks_i_play/
+user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS",250);
+user_pref("general.smoothScroll.msdPhysics.enabled",true);
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant",450);
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",450);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS",50);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio",0.4);
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",5000);
+user_pref("mousewheel.min_line_scroll_amount",40);
+user_pref("toolkit.scrollbox.verticalScrollDistance",5);
+user_pref("toolkit.scrollbox.horizontalScrollDistance",4);
+
